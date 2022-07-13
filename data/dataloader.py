471bb67f861d425f63121ced06c921bdeac7dd77
junsung6140 <junsung0527@gmail.com>
2022-07-14 01:43:17 +0900

    added dncnn trainset

--- data/dataloader.py
+++ data/dataloader.py
@@ -41,3 +41,20 @@ class TestSet(Dataset):
 
     def __getitem__(self, idx):
         return (to_tensor(self.clear[idx//10]), to_tensor(self.hazy[idx]))
+
+class Train_DnCNN(Dataset): 
+    def __init__(self, args):
+        self.args = args
+        with h5py.File(args.train_path, 'r') as f:
+            self.clear = np.array(f['clear'])
+            self.hazy = np.array(f['hazy'])
+
+    def __len__(self):
+        return (10 * len(self.clear))
+
+    def __getitem__(self, idx):
+        clear, hazy = self.clear[idx//10], self.hazy[idx]
+        if self.args.augmentation and np.random.choice([0,1]):
+            clear, hazy= np.flip(clear,1), np.flip(hazy,1)
+        clear, hazy= to_tensor(clear), to_tensor(hazy)
+        return (clear, hazy) 
