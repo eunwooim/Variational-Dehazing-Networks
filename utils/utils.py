f6c69e4530f01595b0394361b650f698914c05af
imeunu <imeunukr@gmail.com>
2022-07-13 20:48:14 +0900

    atmosphere estimation

--- utils/utils.py
+++ utils/utils.py
@@ -8,8 +8,11 @@ def postprocess(output):
     return (output * 255).permute(1,2,0).numpy()
 
 
-def get_A(img):
-    img = np.amin(img, axis=2)
+def get_A(img, p=0.001):
+    dc = np.amin(img, axis=2)
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(31,31))
-    dc = cv2.erode(img, kernel)
-    ##################
+    dc = cv2.erode(dc, kernel)
+    num_pixels = np.prod(dc.shape)
+    flat_img, flat_dc = img.reshape(num_pixels,3), dc.ravel()
+    idx = (-flat_dc).argsort()[:int(num_pixels * p)]
+    return np.max(flat_img.take(idx, axis=0), axis=0)
