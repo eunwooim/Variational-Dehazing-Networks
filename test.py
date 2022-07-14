eae4d26b550ffed43d868fccd433e206e1fbfef1
junsung6140 <junsung0527@gmail.com>
2022-07-14 19:35:18 +0900

    added save image

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import os 
-import torch
 import numpy as np
+import cv2 
+import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader 
 import argparse 
@@ -24,9 +25,11 @@ def set_opts():
     parser.add_argument('--gpu_id', type = int, default = 3)
 
     # model 
-    parser.add_argument('--model_dir', type = str, default='/home/junsung/VHRN/model/model_1.pth')
-    parser.add_argument('--dncnn_dir', type = str, default='/home/junsung/VHRN/model/DnCNN/model_1.pth')
-    parser.add_argument('--mode', type=str, default='dncnn')
+    parser.add_argument('--model_dir', type = str, default='/home/junsung/nas/model/vhrn/model_21.pth')
+    parser.add_argument('--dncnn_dir', type = str, default='/home/junsung/VHRN/model/DnCNN/model_31.pth')
+    parser.add_argument('--mode', type=str, default='')
+    parser.add_argument('--save_img', type = bool, default=True)
+
     args = parser.parse_args()
 
     return args 
@@ -70,6 +73,27 @@ def test():
         total_psnr += psnr_val
 
     print('psnr : ', total_psnr / dataset.__len__() )
+    
+    if args.save_img: 
+        _, im_hazy = [x.cuda().float() for x in data]
+        
+        with torch.no_grad(): 
+            _, im_trans = net(im_hazy, 'train')
+            im_trans = im_trans[:, :3]
+
+        im_hazy = im_hazy.cpu().numpy()
+        im_trans = im_trans.cpu().numpy()
+
+        im_hazy = np.transpose(im_hazy.squeeze(), (1,2,0))
+        im_trans = np.transpose(im_trans.squeeze(), (1,2,0))
+        
+        im_hazy = img_as_ubyte(im_hazy)
+        im_trans = img_as_ubyte(im_trans.clip(0,1))
+        
+        cv2.imwrite('/home/junsung/nas/results/hazy.png', im_hazy)
+        cv2.imwrite('/home/junsung/nas/results/gt.png', im_clear)
+        cv2.imwrite('/home/junsung/nas/results/trans.png', im_trans)
+        cv2.imwrite('/home/junsung/nas/results/dehazy.png', im_dehaze)
 
 def test_dncnn(): 
     # Dataset setting
@@ -109,7 +133,8 @@ def test_dncnn():
         total_psnr += psnr_val
 
     print('psnr : ', total_psnr / dataset.__len__() )
-
+    if args.save_img: 
+        cv2.imwrite('/home/junsung/nas/results/test_dncnn.png', im_dehaze)
 if __name__ == '__main__': 
     args = set_opts()
 
