b4c7a49153be55e9535101acbb3b6380d50d95d6
eunwooim <imeunukr@gmail.com>
2022-07-26 22:07:11 +0900

    update for multiple saving

--- test.py
+++ test.py
@@ -1,150 +1,61 @@
-import os 
+import argparse
+import os
+
+import cv2
 import numpy as np
-import cv2 
+from skimage.metrics import peak_signal_noise_ratio as psnr
 import torch
-import torch.nn as nn
-from torch.utils.data import DataLoader 
-import argparse 
+from torch.utils.data import DataLoader
 
 from data.dataloader import TestSet
 from networks.VHRN import VHRN
-from networks.DnCNN import DnCNN
+from utils import utils
 
-from skimage import img_as_float32, img_as_ubyte
-from skimage.metrics import peak_signal_noise_ratio
 
-def set_opts():
+def test(args, testset):
+    model = utils.load_model(args, VHRN()).cuda()
+    model.eval()
+    total = 0
+    if args.save_img:
+        buffer = []
+
+    for i, data in enumerate(testset):
+        gt, corrupt = [x.cuda().float() for x in data]
+        with torch.no_grad():
+            est = model(corrupt, mode='test')
+        est = utils.postprocess(est.squeeze()[:3])
+        gt = utils.postprocess(gt.squeeze())
+        metric_value = psnr(gt, est)
+        total += metric_value
+        if i+1 in args.save_img:
+            est = cv2.cvtColor(est, cv2.COLOR_BGR2RGB)
+            gt = cv2.cvtColor(gt, cv2.COLOR_BGR2RGB)
+            buffer.append([gt, est])
+            print(f'saved {i+1}')
+    
+    print(f'Metric: {total/(i+1)}')
+
+    if args.save_img:
+        for idx, items in zip(args.save_img, buffer):
+            cv2.imwrite(f'{args.save_path}/gt_{idx}.jpg', items[0])
+            cv2.imwrite(f'{args.save_path}/est_{idx}.jpg', items[1])
+
+def get_args():
     parser = argparse.ArgumentParser()
-    
-    # dataset setttings 
-    parser.add_argument('--train_path', type = str, default= '/home/eunu/nas/reside/in_train.h5')
-    parser.add_argument('--test_path', type = str, default='/home/eunu/nas/reside/in_test.h5')
-    parser.add_argument('--batch_size', type = int, default=1)
+    parser.add_argument('--test_path', type=str, default='/home/eunu/nas/reside/in_test.h5')
+    parser.add_argument('--save_path', type=str, default='/home/eunu/nas/vhrn_test')
 
-    # gpu 
-    parser.add_argument('--gpu_id', type = int, default = 1)
+    parser.add_argument('--save_img', type=tuple, default=(1,50))
+    parser.add_argument('--ckpt', type=str, default='/home/eunu/nas/vhrn_ckpt/1e-7/026.pth')
+    parser.add_argument('--cuda', type=int, default=0)
+    return parser.parse_args()
 
-    # model 
-    parser.add_argument('--model_dir', type = str, default='/home/eunu/VHRN/ckpt/lap/100.pth')
-    parser.add_argument('--dncnn_dir', type = str, default='/home/junsung/VHRN/model/DnCNN/model_31.pth')
-    parser.add_argument('--mode', type=str, default='')
-    parser.add_argument('--save_img', type = bool, default=False)
 
-    args = parser.parse_args()
+if __name__ == '__main__':
+    args = get_args()
+    os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(args.cuda)
 
-    return args 
-
- 
-def test(args):
-    # Dataset setting
-    dataset = TestSet(args)
-    dataloader = DataLoader(dataset = dataset, batch_size = args.batch_size, shuffle=False)
-
-    # model setting 
-    net = VHRN()
-    net = nn.DataParallel(net).cuda()
-    # print(sum(p.numel() for p in net.parameters()))
-
-    checkpoint = torch.load(args.model_dir)
-
-    net.load_state_dict(checkpoint)
-    net.eval()
-
-    total_psnr = 0
-
-    for ii, data in enumerate(dataloader): 
-        im_clear, im_hazy = [x.cuda().float() for x in data]
-
-        with torch.set_grad_enabled(False): 
-            im_dehaze = net(im_hazy, 'test')
-            im_dehaze = im_dehaze[:, :3]
-        # tensor to numpy
-        im_dehaze = im_dehaze.cpu().numpy()
-        im_clear = im_clear.cpu().numpy()
-
-        im_dehaze = np.transpose(im_dehaze.squeeze(), (1,2,0))
-        im_clear = np.transpose(im_clear.squeeze(), (1,2,0))
-
-        # to ubyte 
-        im_dehaze = img_as_ubyte(im_dehaze.clip(0,1))
-        im_clear = img_as_ubyte(im_clear.clip(0,1))
-
-        psnr_val = peak_signal_noise_ratio(im_clear, im_dehaze)
-
-        total_psnr += psnr_val
-
-    print('psnr : ', total_psnr / dataset.__len__() )
-    
-    if args.save_img: 
-        _, im_hazy = [x.cuda().float() for x in data]
-        
-        with torch.no_grad(): 
-            _, im_trans = net(im_hazy, 'train')
-            im_trans = im_trans[:, :3]
-
-        im_hazy = im_hazy.cpu().numpy()
-        im_trans = im_trans.cpu().numpy()
-
-        im_hazy = np.transpose(im_hazy.squeeze(), (1,2,0))
-        im_trans = np.transpose(im_trans.squeeze(), (1,2,0))
-        
-        im_hazy = img_as_ubyte(im_hazy)
-        im_trans = img_as_ubyte(im_trans.clip(0,1))
-        
-        cv2.imwrite('/home/junsung/nas/results/hazy.png', im_hazy)
-        cv2.imwrite('/home/junsung/nas/results/gt.png', im_clear)
-        cv2.imwrite('/home/junsung/nas/results/trans.png', im_trans)
-        cv2.imwrite('/home/junsung/nas/results/dehazy.png', im_dehaze)
-
-def test_dncnn(): 
-    # Dataset setting
-    dataset = TestSet(args)
-    dataloader = DataLoader(dataset = dataset, batch_size = args.batch_size, shuffle=False)
-
-    # model setting 
-    net = DnCNN(in_channels=3, out_channels=3)
-    net = nn.DataParallel(net).cuda()
-
-    checkpoint = torch.load(args.dncnn_dir)
-
-    net.load_state_dict(checkpoint)
-    net.eval()
-
-    total_psnr = 0
-
-    for ii, data in enumerate(dataloader): 
-        im_clear, im_hazy = [x.cuda().float() for x in data]
-
-        with torch.set_grad_enabled(False): 
-            im_dehaze = net(im_hazy)
-            # im_dehaze = im_dehaze[:, :3]
-        # tensor to numpy
-        im_dehaze = im_dehaze.cpu().numpy()
-        im_clear = im_clear.cpu().numpy()
-
-        im_dehaze = np.transpose(im_dehaze.squeeze(), (1,2,0))
-        im_clear = np.transpose(im_clear.squeeze(), (1,2,0))
-
-        # to ubyte 
-        im_dehaze = img_as_ubyte(im_dehaze.clip(0,1))
-        im_clear = img_as_ubyte(im_clear.clip(0,1))
-
-        psnr_val = peak_signal_noise_ratio(im_clear, im_dehaze)
-
-        total_psnr += psnr_val
-
-    print('psnr : ', total_psnr / dataset.__len__() )
-    if args.save_img: 
-        cv2.imwrite('/home/junsung/nas/results/test_dncnn.png', im_dehaze)
-if __name__ == '__main__': 
-    args = set_opts()
-
-    if isinstance(args.gpu_id, int): 
-        os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu_id)
-    else: 
-        os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(str(x)for x in list(args.gpu_id))
-
-    for i in range(100,101):
-        args.model_dir = '/home/eunu/VHRN/ckpt/lap/' + f'{i}.pth'
-        print(i)
-        test(args)
+    testset = TestSet(args)
+    testset = DataLoader(testset, batch_size=1, shuffle=False)
+    test(args, testset)
